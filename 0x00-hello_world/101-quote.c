60138b19ae8c0ca62dab6fe6143dd9bed47ba9c0
Hunchoola <olamideakeem3@gmail.com>
2023-03-16 19:12:45 -0700

    main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - A c program
-*Return 1 (Success)
-*/
+ * main - A c program
+ *Return 1 (Success)
+ i*/
 int main(void)
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
