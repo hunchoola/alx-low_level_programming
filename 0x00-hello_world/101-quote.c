73318bb8f6945bc989fe1b65e1d6ff4fc543ca8b
Hunchoola <olamideakeem3@gmail.com>
2023-03-16 19:15:50 -0700

    main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 /**
  * main - A c program
  *Return 1 (Success)
- i*/
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
