99fc42aff4276fd06438a2f306d232c373077925
Hunchoola <olamideakeem3@gmail.com>
2023-03-20 06:45:08 -0700

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,15 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Kipronoh Vincent
+ */
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
  * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ *        and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
@@ -33,24 +38,4 @@ int main(void)
 
 	return (0);
 
-}
-2 - print_alphabet.c
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
 }
